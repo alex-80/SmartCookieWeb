c8f62f9a4cf5fc5929008ea2adf5054c867adf48
alex <smithalex801@yahoo.com>
2024-02-28 23:00:29 +0800

    Migrate to compose

--- app/src/main/java/com/cookiegames/smartcookie/widget/SearchWidget.kt
+++ app/src/main/java/com/cookiegames/smartcookie/widget/SearchWidget.kt
@@ -11,31 +11,31 @@ import com.cookiegames.smartcookie.R
 
 class SearchWidget : AppWidgetProvider() {
 
-    override fun onUpdate(
-            context: Context,
-            appWidgetManager: AppWidgetManager,
-            appWidgetIds: IntArray
-    ) {
-        // Perform this loop procedure for each App Widget that belongs to this provider
-        appWidgetIds.forEach { appWidgetId ->
-            // Create an Intent to launch ExampleActivity
-            val pendingIntent: PendingIntent = Intent(context, MainActivity::class.java)
-                    .let { intent ->
-                        PendingIntent.getActivity(context, 0, intent, 0)
-                    }
-
-            // Get the layout for the App Widget and attach an on-click listener
-            // to the button
-            val views: RemoteViews = RemoteViews(
-                    context.packageName,
-                    R.layout.search_widget
-            ).apply {
-                //setOnClickPendingIntent(R.id.search_widget_button, pendingIntent)
-            }
-
-
-            // Tell the AppWidgetManager to perform an update on the current app widget
-            appWidgetManager.updateAppWidget(appWidgetId, views)
+  override fun onUpdate(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetIds: IntArray
+  ) {
+    // Perform this loop procedure for each App Widget that belongs to this provider
+    appWidgetIds.forEach { appWidgetId ->
+      // Create an Intent to launch ExampleActivity
+      val pendingIntent: PendingIntent = Intent(context, MainActivity::class.java)
+        .let { intent ->
+          PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
         }
+
+      // Get the layout for the App Widget and attach an on-click listener
+      // to the button
+      val views: RemoteViews = RemoteViews(
+        context.packageName,
+        R.layout.search_widget
+      ).apply {
+        //setOnClickPendingIntent(R.id.search_widget_button, pendingIntent)
+      }
+
+
+      // Tell the AppWidgetManager to perform an update on the current app widget
+      appWidgetManager.updateAppWidget(appWidgetId, views)
     }
+  }
 }
